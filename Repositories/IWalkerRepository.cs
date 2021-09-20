42171ade8bc6c3f0ce65e26d868884ed7baac38d
AlexLam12 <Alex.e015@gmail.com>
2021-09-20 15:57:04 -0500

    initial

---
+++ Repositories/IWalkerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DogGo.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+    }
+}
